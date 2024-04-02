5b9cca36391070e599e3c3457ad571a40623786b
Dave Defourneaux <74273226+dd4no@users.noreply.github.com>
2024-04-01 20:08:41 -0400

    Add Start Menu
    Canvas UI and Menu script

---
+++ Assets/Scripts/DataManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+    public static DataManager instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+}
